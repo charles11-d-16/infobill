b907a9103ecb737796b4f693f12f4066e11d7dc4
Edward Gulmayo <astaltin@gmail.com>
2025-11-17 04:09:16 +0800

    fix: resolve syntax error in DepartmentTransaction schema

--- models/DepartmentTransaction.js
+++ models/DepartmentTransaction.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 
-const departmentTransactionSchema = new mongoose.Schema({
-  categoryId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'DepartmentCategory',
-    required: false
-  },
-  transactions: [
-    {
-      transactionTypeId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'TransactionType'
+const departmentTransactionSchema = new mongoose.Schema(
+  {
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'DepartmentCategory',
+      required: false,
+    },
+    transactions: [
+      {
+        transactionTypeId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'TransactionType',
+        },
       },
       type: {
         type: String
